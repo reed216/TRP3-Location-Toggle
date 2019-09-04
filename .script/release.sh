38a778e1a45fa41f9d9ab1595053d37c45353151
iMintty <imintty@outlook.com>
2019-09-04 22:02:41 +0100

    Added automatic building with travis

---
+++ .script/release.sh
@@ -0,0 +1,2 @@
+curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- -p 297044 -g 8.2.0
+curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- -p 297044 -g 1.13.2
