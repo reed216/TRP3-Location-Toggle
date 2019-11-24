e126e955e4344d7b925296caab523fe4f68eb12c
iMintty <imintty@outlook.com>
2019-11-24 20:26:10 +0000

    Add development builds for easier testing

---
+++ .script/deploy_development.sh
@@ -0,0 +1,2 @@
+curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- -g 8.2.5
+curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- -g 1.13.2
