3b176122c214e9133d534a5a6c4de38ddf4bc606
iMintty <imintty@outlook.com>
2019-11-24 21:47:08 +0000

    Send development builds to discord

--- .script/deploy_development.sh
+++ .script/deploy_development.sh
@@ -1,2 +1,13 @@
+#!/usr/bin/env bash
+
 curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- -g 8.2.5
 curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- -g 1.13.2
+
+curlfiles=""
+for file in "/home/travis/build/iMintty/TRP3-Location-Toggle/.release"/*
+do
+    curlfiles="$curlfiles -F '$(basename $file)=@$file'"
+done
+
+curl $curlfiles $DISCORD_WEBHOOK
+echo $curlfiles
