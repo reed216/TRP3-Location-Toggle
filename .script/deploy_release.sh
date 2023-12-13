7bf61b3f0c634ce2e059a55d2b331254577cb4c9
reed216 <git@reed216.net>
2023-12-13 04:51:32 +0000

    remove travis packaging

--- .script/deploy_release.sh
+++
@@ -1,2 +0,0 @@
-curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- -p 297044 -g 10.1.0
-curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- -p 297044 -g 1.13.5
